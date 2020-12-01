d8a3a97d80d658c811fd19c84b8a16b923706851
Kevin Andrus <kevin.andrus@gmail.com>
2020-12-01 16:58:34 -0500

    Add ResizingIntSet implementation

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -70,36 +70,52 @@ class ResizingIntSet
   end
 
   def insert(num)
-    #self[num] << num unless include?(num)
+    resize! if need_resize_up?
+    unless include?(num)
+      self[num] << num
+      @count += 1
+    end
   end
 
   def remove(num)
-    #self[num].delete(num)
+    @count -= 1 if self[num].delete(num)
+    shrink! if need_resize_down?
   end
 
   def include?(num)
-    #self[num].include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
-    #@store[num % num_buckets]
+    @store[num % num_buckets]
   end
 
   def num_buckets
-    #@store.length
+    @store.length
   end
 
   def resize!
+    nums = @store.flatten
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    nums.each { |num| insert(num) }
+  end
+
+  def shrink!
+    nums = @store.flatten
+    @count = 0
+    @store = Array.new(num_buckets / 2) { Array.new }
+    nums.each { |num| insert(num) }
   end
 
   def need_resize_up?
-    #count >= num_buckets
+    count + 1 >= num_buckets
   end
 
   def need_resize_down?
-    #count < (num_buckets / 3)
+    count <= (num_buckets / 3) && count > 20
   end
 end
