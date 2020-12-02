77d3c1aa187b2819e15c14618bf86863dd26a535
Kevin Andrus <kevin.andrus@gmail.com>
2020-12-02 09:44:54 -0500

    Add solutions for part 3 on making a hash set

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -7,18 +7,26 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      resize! if full?
+      self[key] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    @count -= 1 if self[key].delete(key)
   end
 
   private
 
-  def [](num)
+  def [](key)
     # optional but useful; return the bucket corresponding to `num`
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +34,13 @@ class HashSet
   end
 
   def resize!
+    items = @store.flatten
+    @store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+    items.each { |item| insert(item) }
+  end
+
+  def full?
+    (count + 1) >= num_buckets
   end
 end
