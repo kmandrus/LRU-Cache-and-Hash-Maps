122504b00abed9893ce33bf922a57c8ac1ef6341
Kevin Andrus <kevin.andrus@gmail.com>
2020-12-02 13:45:54 -0500

    Add implementation for part 5 hash_map

--- lib/p05_hash_map.rb
+++ lib/p05_hash_map.rb
@@ -2,6 +2,7 @@ require_relative 'p04_linked_list'
 
 class HashMap
   attr_accessor :count
+  include Enumerable
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
@@ -9,18 +10,37 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    resize! if need_resize?
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    if include?(key)
+      bucket(key).remove(key)
+      @count -= 1
+    end
   end
 
-  def each
+  def each(&block)
+    @store.each do |linked_list|
+      linked_list.each do |node|
+        block.call(node.key, node.val)
+      end
+    end
+
   end
 
   # uncomment when you have Enumerable included
@@ -41,9 +61,19 @@ class HashMap
   end
 
   def resize!
+    pairs = []
+    each { |k, v| pairs << [k, v] }
+    @store = Array.new(@store.size * 2) { LinkedList.new }
+    @count = 0
+    pairs.each { |pair| set(pair.first, pair.last) }
+  end
+
+  def need_resize?
+    (count + 1) >= num_buckets
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
