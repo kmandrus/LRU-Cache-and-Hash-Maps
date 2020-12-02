5a661912d3b2a02975f28784d57ee191de5a52d4
Kevin Andrus <kevin.andrus@gmail.com>
2020-12-02 09:41:33 -0500

    Add solutions for part 2 on hashing

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    diff = self.first.hash
+    self.each_with_index do |int, i|
+      if i > 0
+        diff -=  int.hash
+      end
+    end
+    diff
   end
 end
 
 class String
   def hash
+    chars_to_ints = {}
+    ('a'..'z').to_a.each_with_index do |char, i|
+      chars_to_ints[char] = (i+1)
+    end
+    self.split('').map { |char| chars_to_ints[char] }.hash
   end
 end
 
@@ -16,6 +28,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.flatten.hash
   end
 end
